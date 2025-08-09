3b5fe0bb26f3d91a6ffade4ebdc2cc945f6048fa
unknown <you@example.com>
2025-08-08 23:56:29 -0500

    created controllers for Users

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {sayhello} = require('../controllers/userController');
-const {registerUser} = require("../controllers/userController")
-const {loginUser} = require('../controllers/userController')
+const {registerUser} = require("../controllers/userController");
+const {loginUser} = require('../controllers/userController');
 
 router.get("/sayhello",sayhello);
 router.post("/register",registerUser);
-router.get("login User",loginUser)
+router.post("/loginUser",loginUser)
 
 module.exports = router;
