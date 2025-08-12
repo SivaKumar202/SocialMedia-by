ae15eb495a144711880c5ea93eb8407715e4f577
unknown <you@example.com>
2025-08-11 23:27:37 -0500

    implented All user controllers

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {sayhello, registerUser, loginUser, updateUser } = require('../controllers/userController');
+const {sayhello, registerUser, loginUser, updateUser, getUserProfile } = require('../controllers/userController');
 
 router.get("/sayhello",sayhello);
 router.post("/register",registerUser);
 router.post("/loginUser",loginUser);
-router.put('/update/:id',updateUser)
+router.put('/update/:id',updateUser);
+router.get('/profile/:id',getUserProfile)
 
 module.exports = router;
